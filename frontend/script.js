2fbb30b5caca92538c3ec578f9ccf5c02056d1ec
Bianca Carvalho <caarvalhobianca@gmail.com>
2026-03-10 06:44:09 -0300

    feat (fronted): design inicial do form com bootstrap

--- frontend/script.js
+++ frontend/script.js
@@ -1,232 +1,274 @@
-// simular saída do getAllTasks() do backend
-let taskList = [
-    {
-        id: 1,
-        title: "Tarefa 1",
-        priority: 1,
-        status: "todo",
-        description: "Descrição da tarefa 1",
-        category: "faculdade",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 2,
-        title: "Tarefa 2",
-        priority: 2,
-        status: "doing",
-        description: "Descrição da tarefa 2",
-        category: "casa",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 3,
-        title: "Tarefa 3",
-        priority: 3,
-        status: "done",
-        description: "Descrição da tarefa 3",
-        category: "trabalho",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 4,
-        title: "Tarefa 4",
-        priority: 4,
-        status: "todo",
-        description: "Descrição da tarefa 4",
-        category: "casa",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 5,
-        title: "Tarefa 5",
-        priority: 5,
-        status: "doing",
-        description: "Descrição da tarefa 5",
-        category: "faculdade",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 6,
-        title: "Tarefa 1",
-        priority: 1,
-        status: "todo",
-        description: "Descrição da tarefa 1",
-        category: "faculdade",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 7,
-        title: "Tarefa 2",
-        priority: 2,
-        status: "doing",
-        description: "Descrição da tarefa 2",
-        category: "casa",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 8,
-        title: "Tarefa 3",
-        priority: 3,
-        status: "done",
-        description: "Descrição da tarefa 3",
-        category: "trabalho",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 9,
-        title: "Tarefa 4",
-        priority: 4,
-        status: "todo",
-        description: "Descrição da tarefa 4",
-        category: "casa",
-        dueDate: new Date(2026,5,10)
-    },
-    {
-        id: 10,
-        title: "Tarefa 5",
-        priority: 5,
-        status: "doing",
-        description: "Descrição da tarefa 5",
-        category: "faculdade",
-        dueDate: new Date(2026,5,10)
-    }
-];
+/*
+* ESTADO E DADOS
+* =========================================
+* */
 
-// adicionar colunas de status (sem as tarefas)
-let status = {todo: "A FAZER", doing: "EM PROGRESSO", done: "CONCLUÍDO"};
-let taskListDivInner = "";
+let taskUpdatingId = 0; // recebe o id da task que está sendo atualizada
+let lastId = 10; // variável para setar o id (atribuição do backend, usada aqui para simular)
+let taskList = []; // simular saída do getAllTasks() do backend
 
-for (const key of Object.keys(status)) {
-    taskListDivInner += `
-        <div class="status-column">
-            <div class="column-title">
-                <p>${status[key]}</p>
-                <i class="fa-regular fa-plus"></i>
+let status = {todo: "A FAZER", doing: "EM PROGRESSO", done: "CONCLUÍDO"}; // mapa de status
+let priorityIconStyle = [
+    { value: 1, class: "fa-solid fa-angles-down", color: "dodgerblue" },
+    { value: 2, class: "fa-solid fa-angle-down", color: "dodgerblue" },
+    { value: 3, class: "fa-solid fa-equals", color: "darkorange" },
+    { value: 4, class: "fa-solid fa-angle-up", color: "darkred" },
+    { value: 5, class: "fa-solid fa-angles-up", color: "darkred" }
+]; // prioridades e classes dos ícones que representam cada uma
+
+/*
+ * RENDERIZAÇÃO HTML
+ * =========================================
+ */
+
+// -> modal do bootstrap
+let taskModal;
+
+document.addEventListener("DOMContentLoaded", function() {
+    taskModal = new bootstrap.Modal(document.getElementById('taskModal'));
+});
+
+// -> função para criar card de tarefa
+function createTaskCard(task)  {
+    let priorityStyle = priorityIconStyle.find(el => el.value === task.priority);
+    let dueDateTag = `
+        <p class="due-date">
+            <i class="fa-regular fa-clock"></i>
+            ${task.dueDate ? task.dueDate.toLocaleDateString() : ""}
+        </p>
+    `;
+    let categoryTag = `<p class="category">${task.category}</p>`
+    return `
+        <div id="task-card-${task.id}" class="task-mini-card">
+            <div class="header-task">
+                <p>${task.title}</p>
+                <div>
+                    <i id="update-task" class="fa-regular fa-pen-to-square" onclick="updateTask(${task.id})"></i>
+                    <i id="delete-task" class="fa-solid fa-trash" onclick="deleteTask(${task.id})"></i>
+                </div>
             </div>
-            <!-- todo - um laço para adicionar os cards de tarefa aqui -->
-            <div id="${key}-column">
+            <div class="footer-task">
+                <div>
+                    ${task.dueDate ? dueDateTag : ""}
+                    ${task.category ? categoryTag : ""}
+                </div>
+                <p class="priority-${task.priority}">
+                    <i class="${priorityStyle.class}" style="color: ${priorityStyle.color}"></i>
+                </p>
             </div>
         </div>
-    `
+    `;
 }
 
-document.getElementById('task-list-div').innerHTML = taskListDivInner;
+// -> função para adicionar novo card de tarefa na respectiva coluna
+function addTaskCardToColumn(task) {
+    document.getElementById(`${task.status}-column`).innerHTML += createTaskCard({
+        id: task.id,
+        title: task.title,
+        dueDate: task.dueDate,
+        priority: task.priority,
+        category: task.category
+    });
+}
 
-// adicionar as tarefas nas colunas
-let todoTasks = "";
-let doingTasks = "";
-let doneTasks = "";
-for (const task of taskList) {
-    if (task.status === "todo") {
-        todoTasks += `
-            <div class="task-mini-card">
-                <div class="header-task">
-                    <p>${task.title}</p>
-                    <div>
-                        <i class="fa-regular fa-pen-to-square"></i>
-                        <i class="fa-solid fa-trash"></i>
-                    </div>
-                </div>
-                <div class="footer-task">
-                    <div>
-                        <p class="due-date">
-                            <i class="fa-regular fa-clock"></i>
-                            ${task.dueDate.toLocaleDateString()}
-                        </p>
-                        <p class="category">${task.category}</p>
-                    </div>
-                    <p class="priority-${task.priority}">
-                        <i></i>
-                    </p>
-                </div>
-            </div>
-        `
-    }
+// -> função para remover card de tarefa
+function removeTaskCard(id) {
+    document.getElementById(`task-card-${id}`).remove();
+}
 
-    if (task.status === "doing") {
-        doingTasks += `
-            <div class="task-mini-card">
-                <div class="header-task">
-                    <p>${task.title}</p>
-                    <div>
-                        <i class="fa-regular fa-pen-to-square"></i>
-                        <i class="fa-solid fa-trash"></i>
-                    </div>
-                </div>
-                <div class="footer-task">
-                    <div>
-                        <p class="due-date">
-                            <i class="fa-regular fa-clock"></i>
-                            ${task.dueDate.toLocaleDateString()}
-                        </p>
-                        <p class="category">${task.category}</p>
-                    </div>
-                    <p class="priority-${task.priority}">
-                        <i></i>
-                    </p>
-                </div>
-            </div>
-        `
-    }
+/*
+* MANIPULAÇÃO DO FORM/MODAL
+* =========================================
+* */
 
-    if (task.status === "done") {
-        doneTasks += `
-            <div class="task-mini-card">
-                <div class="header-task">
-                    <p>${task.title}</p>
-                    <div>
-                        <i class="fa-regular fa-pen-to-square"></i>
-                        <i class="fa-solid fa-trash"></i>
-                    </div>
-                </div>
-                <div class="footer-task">
-                    <div>
-                        <p class="due-date">
-                            <i class="fa-regular fa-clock"></i>
-                            ${task.dueDate.toLocaleDateString()}
-                        </p>
-                        <p class="category">${task.category}</p>
-                    </div>
-                    <p class="priority-${task.priority}">
-                        <i></i>
-                    </p>
-                </div>
-            </div>
-        `
+// -> função para pegar os valores dos campos do form
+function getInputData() {
+    return {
+        title: document.getElementById("title-input").value,
+        priority: parseInt(document.getElementById("priority-input").value),
+        status: document.getElementById("status-input").value,
+        dueDate: document.getElementById("dueDate-input").value ? new Date(document.getElementById("dueDate-input").value + 'T00:00:00') : "",
+        category: document.getElementById("category-input").value,
+        description: document.getElementById("description-input").value
     }
 }
 
-document.getElementById("todo-column").innerHTML = todoTasks;
-document.getElementById("doing-column").innerHTML = doingTasks;
-document.getElementById("done-column").innerHTML = doneTasks;
+// -> função para resetar os dados do form (limpa tudo)
+function clearInputData() {
+    document.getElementById("title-input").value = "";
+    document.getElementById('priority-input').value = "";
+    document.getElementById("status-input").value = "";
+    document.getElementById("dueDate-input").value = "";
+    document.getElementById("category-input").value = "";
+    document.getElementById("description-input").value = "";
 
-// setando os icones de prioridade
-let priority1Elements = document.getElementsByClassName("priority-1")
-for (const el of priority1Elements) {
-    el.getElementsByTagName("i")[0].setAttribute("class", "fa-solid fa-angles-down");
-    el.getElementsByTagName("i")[0].style.color = "dodgerblue";
+    document.getElementById("priority-input").value = "";
+    let spanBtn = document.getElementById('priority-selected-text');
+    spanBtn.innerHTML = "Selecione uma prioridade...";
+    spanBtn.classList.add('text-secondary');
 }
 
-let priority2Elements = document.getElementsByClassName("priority-2")
-for (const el of priority2Elements) {
-    el.getElementsByTagName("i")[0].setAttribute("class", "fa-solid fa-angle-down");
-    el.getElementsByTagName("i")[0].style.color = "dodgerblue";
+// -> seta valores nos campos do form (usado para atualização de tarefa)
+function setInputData(task) {
+    let year = task.dueDate.getFullYear();
+    let month = String(task.dueDate.getMonth() + 1).padStart(2, '0');
+    let day = String(task.dueDate.getDate()).padStart(2, '0');
+    let formattedDate = `${year}-${month}-${day}`;
+
+    document.getElementById("title-input").value = task.title;
+    document.getElementById("status-input").value = task.status;
+    document.getElementById("dueDate-input").value = formattedDate;
+    document.getElementById("category-input").value = task.category;
+    document.getElementById("description-input").value = task.description;
+
+    document.getElementById("priority-input").value = task.priority;
+    let selectedItem = document.querySelector(`.dropdown-menu .dropdown-item[data-value="${task.priority}"]`);
+    let spanBtn = document.getElementById('priority-selected-text');
+    if (selectedItem) {
+        spanBtn.innerHTML = selectedItem.innerHTML;
+        spanBtn.classList.remove('text-secondary');
+    }
 }
 
-let priority3Elements = document.getElementsByClassName("priority-3")
-for (const el of priority3Elements) {
-    el.getElementsByTagName("i")[0].setAttribute("class", "fa-solid fa-equals");
-    el.getElementsByTagName("i")[0].style.color = "darkorange";
+/*
+* EVENTOS ONCLICK DO FORM/MODAL
+* =========================================
+* */
+
+// -> fechar o modal pelo x
+document.querySelector("#taskModal .close-btn").onclick = function () {
+    taskModal.hide();
+    clearInputData();
 }
 
-let priority4Elements = document.getElementsByClassName("priority-4")
-for (const el of priority4Elements) {
-    el.getElementsByTagName("i")[0].setAttribute("class", "fa-solid fa-angle-up");
-    el.getElementsByTagName("i")[0].style.color = "darkred";
+// -> cancelar criação/atualização pelo botão cancelar
+document.querySelector("#taskModal .btn-cancel").onclick = function () {
+    taskModal.hide();
+    clearInputData();
 }
 
-let priority5Elements = document.getElementsByClassName("priority-5")
-for (const el of priority5Elements) {
-    el.getElementsByTagName("i")[0].setAttribute("class", "fa-solid fa-angles-up");
-    el.getElementsByTagName("i")[0].style.color = "darkred";
+// -> criar tarefa (botão criar)
+document.querySelector("#taskModal .btn-create").onclick = function (event) {
+    let form = document.getElementById("form-task");
+
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
+
+    event.preventDefault();
+
+    taskModal.hide();
+
+    let newTask = getInputData();
+    newTask.id = lastId + 1;
+    lastId += 1;
+
+    taskList.push(newTask);
+    addTaskCardToColumn(newTask);
+    clearInputData();
 }
+
+// -> editar tarefa (botão atualizar)
+document.querySelector("#taskModal .btn-update").onclick = function (event) {
+    let form = document.getElementById("form-task");
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
+
+    event.preventDefault();
+
+    taskModal.hide();
+
+    let newData = getInputData();
+
+    taskList.map((task) => {
+        if(task.id === taskUpdatingId) {
+            task.title = newData.title;
+            task.status = newData.status;
+            task.priority = newData.priority;
+            task.category = newData.category;
+            task.dueDate = newData.dueDate;
+            task.description = newData.description;
+        }
+    })
+
+    let updatedTask = taskList.find(t => t.id === taskUpdatingId);
+
+    removeTaskCard(taskUpdatingId);
+    addTaskCardToColumn(updatedTask)
+    clearInputData();
+}
+
+/*
+* EVENTOS ONCLICK CARDS E COLUNAS (CRIAR, ATUALIZAR, REMOVER)
+* =========================================
+* */
+
+// -> criar tarefa
+for (const key of Object.keys(status)) {
+    document.getElementById(`create-${key}-task`).onclick = function () {
+        taskUpdatingId = 0;
+        document.getElementById("status-input").value = key;
+        $('#status-input').trigger('change');
+
+        document.getElementById("modal-title").innerHTML = "Criar Nova Tarefa";
+
+        document.querySelector(".btn-create").style.display = "git logblock";
+        document.querySelector(".btn-update").style.display = "none";
+
+        taskModal.show();
+    }
+}
+
+// -> remover tarefa
+function deleteTask(id) {
+    let index = taskList.findIndex(t => t.id === id);
+    if (index !== -1) {
+        taskList.splice(index,1);
+        removeTaskCard(id);
+    } else {
+        console.log("index nao encontrado");
+    }
+}
+
+// -> atualizar tarefa
+function updateTask(id) {
+    taskUpdatingId = id;
+    document.getElementById("modal-title").innerHTML = "Atualizar Tarefa";
+    document.querySelector(".btn-create").style.display = "none";
+    document.querySelector(".btn-update").style.display = "block";
+
+    let task = taskList.find(t => t.id === id);
+    if (task) {
+        setInputData({
+            title: task.title,
+            dueDate: task.dueDate,
+            priority: task.priority,
+            category: task.category,
+            status: task.status,
+            description: task.description
+        });
+        taskModal.show();
+    } else {
+        console.log("tarefa nao encontrada");
+    }
+}
+
+document.querySelectorAll('.dropdown-menu .dropdown-item').forEach(item => {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let selectedValue = this.getAttribute('data-value');
+        let selectedHTML = this.innerHTML;
+
+        // Atualiza o input escondido com o número (1, 2, etc)
+        document.getElementById('priority-input').value = selectedValue;
+
+        // Atualiza o visual do botão (tira a cor cinza do placeholder e coloca o ícone+texto)
+        let spanBtn = document.getElementById('priority-selected-text');
+        spanBtn.innerHTML = selectedHTML;
+        spanBtn.classList.remove('text-secondary');
+    });
+});
